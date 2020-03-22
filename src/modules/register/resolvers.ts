c10a276ba11feb234554aef9da4a3c1da9442227
jussaurabh <pawar.saurabh95@gmail.com>
2020-03-22 17:50:18 +0530

    handling errors in graphql

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -7,12 +7,26 @@ export const resolvers = {
 	},
 	Mutation: {
 		register: async (_, { email, password }) => {
+			const userAlreadyExists = await User.findOne({
+				where: { email },
+				select: [ 'id' ]
+			});
+
+			if (userAlreadyExists) {
+				return [
+					{
+						path: 'email',
+						message: 'User already exists'
+					}
+				];
+			}
+
 			const hashedPassword = await bcrypt.hash(password, 10);
 			await User.insert({
 				email,
 				password: hashedPassword
 			});
-			return true;
+			return null;
 		}
 	}
 };
