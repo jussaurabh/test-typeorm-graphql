2430a214a330e1672b08df7a15e46ceb6e5153c9
jussaurabh <pawar.saurabh95@gmail.com>
2020-03-22 17:29:14 +0530

    basic registration setup with multiple resolvers and schemas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+// BASIC - basic-register-setup
+/**
+ * this branch contains the basic setup for registering user
+ * with Setup of creating multiple resolvers and schemas
+ * and merging them using graphql-tools(makeExecutableSchemas, mergeSchemas)
+ */
+
 import 'reflect-metadata';
 import { startServer } from './startServer';
 
